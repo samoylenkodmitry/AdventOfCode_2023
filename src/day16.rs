62199863b61524b2e55632706703e98efce310ac
samoylenkodmitry <samoylenkodmitry@gmail.com>
2023-12-16 21:15:25 +0300

    Day16 part 2
    Signed-off-by: samoylenkodmitry <samoylenkodmitry@gmail.com>

--- src/day16.rs
+++ src/day16.rs
@@ -16,19 +16,19 @@ pub(crate) fn day16() {
 .|....-|.\
 ..//.|....
 */
-    let example_lines =
-        vec![
-            ".|...\\....",
-            "|.-.\\.....",
-            ".....|-...",
-            "........|.",
-            ".........",
-            ".........\\",
-            "..../.\\\\..",
-            ".-.-/..|..",
-            ".|....-|.\\",
-            "..//.|....",
-        ];
+    let raw_str =
+r###".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....
+"###;
+    let example_lines:Vec<&str> = raw_str.lines().collect();
     // convert example lines to String
     let example_lines: Vec<String> =
         example_lines.iter().map(|s| s.to_string()).collect();
@@ -38,7 +38,21 @@ pub(crate) fn day16() {
     let input = std::fs::read_to_string("./inputs/day16.txt").unwrap();
     // split input into lines
     let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
-    part1(input);
+    //part1(input);
+
+    // part 2
+
+    let example_lines:Vec<&str> = raw_str.lines().collect();
+    // convert example lines to String
+    let example_lines: Vec<String> =
+        example_lines.iter().map(|s| s.to_string()).collect();
+
+    part2(example_lines);
+
+    let input = std::fs::read_to_string("./inputs/day16.txt").unwrap();
+    // split input into lines
+    let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+    part2(input);
 }
 
 fn part1(lines: Vec<String>) {
@@ -54,15 +68,21 @@ beam     -->.|...\....
             .|....-|.\
             ..//.|....
 */
+    let initial_beam = (0, 0, 'r');
     let mut grid: Vec<Vec<char>> = lines.iter().map(|s| s.chars().collect()).collect();
-    // let's do a BFS and remember each visited node with a HashSet
+
+    let count_visited = fire_beam(initial_beam, &mut grid);
+    println!("Part 1: {}", count_visited);
+}
+
+fn fire_beam(initial_beam: (usize, usize, char), grid: &mut Vec<Vec<char>>) -> i32 {
+// let's do a BFS and remember each visited node with a HashSet
     // we must store (x, y, beam_direction) in the HashSet
     // we will encode direction as a char: 'u', 'd', 'l', 'r'
     let mut visited: HashSet<(usize, usize, char)> = HashSet::new();
     // we will use a Queue to store the next nodes to visit
     let mut queue: Vec<(usize, usize, char)> = Vec::new();
-    // beam starts at (0, 0) and goes to the right
-    queue.push((0, 0, 'r'));
+    queue.push(initial_beam);
     // we don't need to store path
     // let's however, store count of visits for each cell
     let mut count: HashMap<(usize, usize), usize> = HashMap::new();
@@ -92,7 +112,7 @@ beam     -->.|...\....
             ('u', '-') => {
                 if x > 0 { queue.push((x - 1, y, 'l')) };
                 if x < last_x { queue.push((x + 1, y, 'r')) };
-            }
+            },
             ('u', '.') => if y > 0 { queue.push((x, y - 1, 'u')) },
             ('d', '/') => if x > 0 { queue.push((x - 1, y, 'l')) },
             ('d', '\\') => if x < last_x { queue.push((x + 1, y, 'r')) },
@@ -115,7 +135,7 @@ beam     -->.|...\....
             ('r', '|') => {
                 if y > 0 { queue.push((x, y - 1, 'u')) };
                 if y < last_y { queue.push((x, y + 1, 'd')) };
-            }
+            },
             ('r', '-') => if x < last_x { queue.push((x + 1, y, 'r')) },
             ('r', '.') => if x < last_x { queue.push((x + 1, y, 'r')) },
 
@@ -129,7 +149,48 @@ beam     -->.|...\....
             count_visited += 1;
         }
     }
-    println!("Part 1: {}", count_visited);
+    count_visited
 }
 
-fn part2(lines: Vec<String>) {}
+fn part2(lines: Vec<String>) {
+
+    /*
+beam     -->.|...\....
+            |.-.\.....
+            .....|-...
+            ........|.
+            ..........
+            .........\
+            ..../.\\..
+            .-.-/..|..
+            .|....-|.\
+            ..//.|....
+*/
+    let mut grid: Vec<Vec<char>> = lines.iter().map(|s| s.chars().collect()).collect();
+    let mut max_count = 0;
+    let directions = vec!['u', 'd', 'l', 'r'];
+    // try to fire all beams from the sides
+    for y in 0..grid.len() {
+        let count_visited = fire_beam((0, y, 'r'), &mut grid);
+        if count_visited > max_count {
+            max_count = count_visited;
+        }
+        let beam = (grid[0].len() - 1, y, 'l');
+        let count_visited = fire_beam(beam, &mut grid);
+        if count_visited > max_count {
+            max_count = count_visited;
+        }
+    }
+    for x in 0..grid[0].len() {
+        let count_visited = fire_beam((x, 0, 'd'), &mut grid);
+        if count_visited > max_count {
+            max_count = count_visited;
+        }
+        let count_visited = fire_beam((x, grid.len() - 1, 'u'), &mut grid);
+        if count_visited > max_count {
+            max_count = count_visited;
+        }
+    }
+
+    println!("Part 2: {}", max_count);
+}
