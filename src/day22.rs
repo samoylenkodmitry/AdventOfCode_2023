6ad942d651d15dfc0f665efc5b6a8c07978dff7b
samoylenkodmitry <samoylenkodmitry@gmail.com>
2023-12-22 19:25:18 +0300

    Day22 part 1 - werks
    Signed-off-by: samoylenkodmitry <samoylenkodmitry@gmail.com>

--- src/day22.rs
+++ src/day22.rs
@@ -21,12 +21,56 @@ pub(crate) fn day22() {
     let example_lines: Vec<String> =
         example_lines.iter().map(|s| s.to_string()).collect();
 
-    part1(example_lines);
+    //part1(example_lines);
 
     let input = std::fs::read_to_string("./inputs/day22.txt").unwrap();
     //// split input into lines
     let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
     part1(input);
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Brick {
+    x1: u16,
+    y1: u16,
+    z1: u16,
+    x2: u16,
+    y2: u16,
+    z2: u16,
+}
+
+impl Brick {
+    fn from_str(s: &str) -> Brick {
+        let mut nums: Vec<u16> = s.split([',', '~']).map(|s| s.parse::<u16>().unwrap()).collect();
+        if nums.len() != 6 {
+            panic!("nums.len() != 6");
+        }
+        if nums[2] > nums[5] {
+            panic!("nums[2] > nums[5]");
+        }
+        Brick {
+            x1: nums[0],
+            y1: nums[1],
+            z1: nums[2],
+            x2: nums[3],
+            y2: nums[4],
+            z2: nums[5],
+        }
+    }
+    fn intersects(&self, other: &Self) -> bool {
+        !(self.x2 < other.x1 || self.x1 > other.x2 ||
+            self.y2 < other.y1 || self.y1 > other.y2 ||
+            self.z2 < other.z1 || self.z1 > other.z2)
+    }
+    fn intersects_xy(&self, other: &Self) -> bool {
+        !(self.x2 < other.x1 || self.x1 > other.x2 ||
+            self.y2 < other.y1 || self.y1 > other.y2)
+    }
+}
+
+fn parse_bricks(lines: Vec<String>) -> Vec<Brick> {
+    lines.iter().map(|line| Brick::from_str(line)).collect()
 }
 
 fn part1(lines: Vec<String>) {
@@ -40,9 +84,7 @@ fn part1(lines: Vec<String>) {
 1,1,8~1,1,9"###;
      */
     // let's parse bricks coordinates [x1,y1,z1,x2,y2,z2], all numbers are 0..=512
-    let mut bricks: Vec<Vec<u16>> = lines.iter().map(|line| {
-        line.split(['~', ',']).map(|s| s.parse::<u16>().unwrap()).collect()
-    }).collect();
+    let mut bricks: Vec<Brick> = parse_bricks(lines);
 
     /*
     Now we need to make all the bricks fall down on 'z' axis
@@ -52,9 +94,9 @@ fn part1(lines: Vec<String>) {
     another brick or the ground
     We can use a HashSet to store the bricks that are on the ground
      */
-    let mut bricks_under: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut brick_to_bottoms: HashMap<usize, HashSet<usize>> = HashMap::new();
     // and inverted HashMap
-    let mut bricks_over: HashMap<usize, HashSet<usize>> = HashMap::new();
+    let mut brick_to_tops: HashMap<usize, HashSet<usize>> = HashMap::new();
     let mut stopped_bricks: HashSet<usize> = HashSet::new();
     // let's sort the bricks by their 'y' coordinate
     let mut bricks_indices: Vec<usize> = (0..bricks.len()).collect();
@@ -62,13 +104,13 @@ fn part1(lines: Vec<String>) {
         // each Vec has 6 coordinates, x, y, z, x, y, z
         let a = &bricks[*a];
         let b = &bricks[*b];
-        a[2].cmp(&b[2])
+        a.z1.cmp(&b.z1)
     });
     // now we can fall bricks in order of their 'y' coordinate
     for brick_fall_ind in bricks_indices {
         let brick_fall = &bricks[brick_fall_ind];
         // if brick is already on the ground, add it to stopped_bricks, then continue
-        if brick_fall[2] == 0 {
+        if brick_fall.z1 == 0 {
             stopped_bricks.insert(brick_fall_ind);
             continue;
         }
@@ -81,22 +123,28 @@ fn part1(lines: Vec<String>) {
         // find all that intersect with the falling brick
         // then find all with the max z2 coordinate
 
-        let (x1, y1, z1, x2, y2, z2) = (brick_fall[0], brick_fall[1], brick_fall[2],
-                                        brick_fall[3], brick_fall[4], brick_fall[5]);
-        // we can use a HashMap<z2, ind> to store the bricks that are under the falling brick
-        let mut bricks_under_fall: HashMap<u16, Vec<usize>> = HashMap::new();
+        // we can use a Vec<ind> to store the bricks that are under the falling brick
+        // and intersect it
+        let mut bricks_under_fall: HashSet<usize> = HashSet::new();
         let mut sz2_max = 0;
         for stopped_brick_ind in &stopped_bricks {
             let stopped_brick = &bricks[*stopped_brick_ind];
             // check if stopped_brick intersects with brick_fall
             // on XY plane bricks coordinates are rectangular x1,y1,x2,y2
-            let (sx1, sy1, sz1, sx2, sy2, sz2) = (stopped_brick[0], stopped_brick[1], stopped_brick[2],
-                                                  stopped_brick[3], stopped_brick[4], stopped_brick[5]);
-            if intersect(x1, y1, x2, y2, sx1, sy1, sx2, sy2) {
+            if stopped_brick.intersects_xy(brick_fall) {
+                if stopped_brick.z2 > brick_fall.z1 {
+                    // if stopped_brick is above the falling brick
+                    panic!("sz2 > z1");
+                }
                 // if stopped_brick intersects with brick_fall
                 // add it to bricks_under_fall
-                bricks_under_fall.entry(sz2).or_insert(Vec::new()).push(*stopped_brick_ind);
-                sz2_max = max(sz2_max, sz2);
+                if stopped_brick.z2 > sz2_max {
+                    sz2_max = stopped_brick.z2;
+                    bricks_under_fall.clear();
+                    bricks_under_fall.insert(*stopped_brick_ind);
+                } else if stopped_brick.z2 == sz2_max {
+                    bricks_under_fall.insert(*stopped_brick_ind);
+                }
             }
         }
         println!("sz2_max: {}", sz2_max);
@@ -107,95 +155,45 @@ fn part1(lines: Vec<String>) {
             // modify the brick_fall coordinates z1 and z2
             // and add it to stopped_bricks
             let brick_fall = &mut bricks[brick_fall_ind];
-            let diff = z2 - z1;
-            brick_fall[2] = sz2_max + 1;
-            brick_fall[5] = sz2_max + diff + 1;
+            let diff = brick_fall.z2 - brick_fall.z1;
+            brick_fall.z1 = sz2_max + 1;
+            brick_fall.z2 = sz2_max + diff + 1;
             stopped_bricks.insert(brick_fall_ind);
             continue;
         }
         // now we have all the bricks that are under the falling brick
         // we can add them to bricks_under
-        let x3 = bricks_under_fall.get(&sz2_max).unwrap();
-        let bricks_under_fall_set: HashSet<usize> = x3.iter().map(|x| *x).collect();
-        bricks_under.insert(brick_fall_ind, bricks_under_fall_set.clone());
+        brick_to_bottoms.insert(brick_fall_ind, bricks_under_fall.clone());
         // and add the falling brick to bricks_over
-        for brick_under_fall_ind in bricks_under_fall_set {
-            bricks_over.entry(brick_under_fall_ind).or_insert(HashSet::new()).insert(brick_fall_ind);
+        // let's check that z2 is the same for all bricks_under_fall
+        let mut sz2 = u16::MAX;
+        for brick_under_fall_ind in &bricks_under_fall {
+            let brick_under_fall = &bricks[*brick_under_fall_ind];
+            if sz2 == u16::MAX {
+                sz2 = brick_under_fall.z2;
+            } else if brick_under_fall.z2 != sz2 {
+                panic!("brick_under_fall[5] != sz2");
+            }
+        }
+        for brick_under_fall_ind in bricks_under_fall {
+            brick_to_tops.entry(brick_under_fall_ind).or_insert(HashSet::new()).insert(brick_fall_ind);
         }
         // finally modify the brick_fall coordinates z1 and z2
         // and add it to stopped_bricks
         let brick_fall = &mut bricks[brick_fall_ind];
-        let diff = z2 - z1;
-        brick_fall[2] = sz2_max + 1;
-        brick_fall[5] = sz2_max + diff + 1;
+        let diff = brick_fall.z2 - brick_fall.z1;
+        brick_fall.z1 = sz2_max + 1;
+        brick_fall.z2 = sz2_max + diff + 1;
         stopped_bricks.insert(brick_fall_ind);
     }
 
-    println!("bricks: {:?}", bricks);
-    // print XZ of bricks
-    for z in (0..9).rev() {
-        for x in 0..=2 {
-            let mut found = 0;
-            let mut found_ind = -1;
-            for (ind, brick) in bricks.iter().enumerate() {
-                if brick[0] <= x && x <= brick[3] && brick[2] <= z && z <= brick[5] {
-                    found += 1;
-                    found_ind = ind as i32;
-                }
-            }
-            if found == 0 {
-                print!(".");
-            } else if found == 1 {
-                print!("{}", found_ind);
-            } else {
-                print!("?");
-            }
-        }
-        println!();
-    }
-    println!("---");
-    // print YZ of bricks
-    for z in (0..9).rev() {
-        for y in 0..=2 {
-            let mut found = 0;
-            let mut found_ind = -1;
-            for (ind, brick) in bricks.iter().enumerate() {
-                if brick[1] <= y && y <= brick[4] && brick[2] <= z && z <= brick[5] {
-                    found += 1;
-                    found_ind = ind as i32;
-                }
-            }
-            if found == 0 {
-                print!(".");
-            } else if found == 1 {
-                print!("{}", found_ind);
-            } else {
-                print!("?");
-            }
-        }
-        println!();
-    }
-    println!("bricks_under: {:?}", bricks_under);
-    println!("bricks_over: {:?}", bricks_over);
-
-    // now we need to count the number of bricks that can be safely removed in one step
-    // if brick lies on just a single brick, that supporting brick can NOT be removed
-    // however if brick lies on multiple bricks, all of them can be removed
-    // we need to analyze who is supporting who
-
-    // start from simple, let's count all bricks that have no bricks on top of them
-    let mut bricks_no_over: HashSet<usize> = HashSet::new();
-    for brick_ind in 0..bricks.len() {
-        if !bricks_over.contains_key(&brick_ind) {
-            bricks_no_over.insert(brick_ind);
-        }
-    }
-    println!("bricks_no_over: {:?}", bricks_no_over);
+    println!("bricks_under: {:?}", brick_to_bottoms);
+    println!("bricks_over: {:?}", brick_to_tops);
 
     // now let's count all bricks that have only single brick below them, let's remember that brick
     // we can use bricks_under HashMap for that
     let mut bricks_single_under: HashSet<usize> = HashSet::new();
-    for (brick_ind, bricks_under_set) in &bricks_under {
+    for (brick_ind, bricks_under_set) in &brick_to_bottoms {
         if bricks_under_set.len() == 1 {
             bricks_single_under.insert(bricks_under_set.iter().next().unwrap().clone());
         }
@@ -207,26 +205,23 @@ fn part1(lines: Vec<String>) {
     println!("Part 1: {}", count);
 }
 
-// check if two rectangles intersect
-// x1,y1,x2,y2 and sx1,sy1,sx2,sy2
-fn intersect(x1: u16, y1: u16, x2: u16, y2: u16, sx1: u16, sy1: u16, sx2: u16, sy2: u16) -> bool {
-    // check if x1,y1 is inside sx1,sy1,sx2,sy2
-    if x1 >= sx1 && x1 <= sx2 && y1 >= sy1 && y1 <= sy2 {
-        return true;
-    }
-    // check if x2,y2 is inside sx1,sy1,sx2,sy2
-    if x2 >= sx1 && x2 <= sx2 && y2 >= sy1 && y2 <= sy2 {
-        return true;
-    }
-    // check if sx1,sy1 is inside x1,y1,x2,y2
-    if sx1 >= x1 && sx1 <= x2 && sy1 >= y1 && sy1 <= y2 {
-        return true;
-    }
-    // check if sx2,sy2 is inside x1,y1,x2,y2
-    if sx2 >= x1 && sx2 <= x2 && sy2 >= y1 && sy2 <= y2 {
-        return true;
-    }
-    false
-}
-
 fn part2(input: &str) {}
+
+#[cfg(test)]
+mod tests_day22 {
+    use super::*;
+
+    #[test]
+    fn test_day22_part1_intersect_same_z() {
+        let brick1 = Brick::from_str("0,0,0~1,1,0");
+        let brick2 = Brick::from_str("0,0,0~1,1,0");
+        assert!(brick1.intersects(&brick2));
+    }
+
+    #[test]
+    fn test_day22_part1_intersect_xy_different_z() {
+        let brick1 = Brick::from_str("0,0,0~1,1,0");
+        let brick2 = Brick::from_str("0,0,1~1,1,1");
+        assert!(brick1.intersects_xy(&brick2));
+    }
+}
