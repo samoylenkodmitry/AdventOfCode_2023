f100ba33ba7a8c12655b4fb70ad55667c4004aad
samoylenkodmitry <samoylenkodmitry@gmail.com>
2023-12-23 17:19:50 +0300

    Day 23 part 2 werks
    Signed-off-by: samoylenkodmitry <samoylenkodmitry@gmail.com>

--- src/day23.rs
+++ src/day23.rs
@@ -43,6 +43,19 @@ pub(crate) fn day23() {
     //// split input into lines
     let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
     part1(input);
+
+    // part 2
+    let example_lines: Vec<&str> = raw_str.lines().collect();
+    // convert example lines to String
+    let example_lines: Vec<String> =
+        example_lines.iter().map(|s| s.to_string()).collect();
+
+    part2(example_lines);
+
+    let input = std::fs::read_to_string("./inputs/day23.txt").unwrap();
+    //// split input into lines
+    let input: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+    part2(input);
 }
 
 fn part1(lines: Vec<String>) {
@@ -110,8 +123,8 @@ fn part1(lines: Vec<String>) {
                     .filter(|&&(x1, y1)| !(x1 == x && y1 == y))
                     .filter(|&&(x, y)| grid[y][x] != '#')
                     .for_each(|&(x, y)| {
-                    queue.push_back((x, y, visited.clone()));
-                });
+                        queue.push_back((x, y, visited.clone()));
+                    });
             }
             '<' => {
                 // must go left
@@ -144,4 +157,119 @@ fn part1(lines: Vec<String>) {
     println!("part1: {}", max_path - 1);
 }
 
-fn part2(lines: Vec<String>) {}
+fn part2(lines: Vec<String>) {
+    // find the longest path from top row to bottom row
+    let grid: Vec<Vec<char>> = lines.iter().map(|s| s.chars().collect()).collect();
+    // ok, BFS is not going to work here
+    // we need to collapse the grid into a graph
+    // any point with more than 2 neighbors is a node
+    // to fill in the nodes, we need to do a BFS
+    let mut nodes_set: HashSet<(usize, usize)> = HashSet::new();
+    let mut nodes_vec: Vec<(usize, usize)> = Vec::new();
+    let mut grid_to_node_ind: HashMap<(usize, usize), usize> = HashMap::new();
+    // find all nodes
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == '#' {
+                continue;
+            }
+            let mut neighbors = 0;
+            if x > 0 && grid[y][x - 1] != '#' {
+                neighbors += 1;
+            }
+            if x < grid[0].len() - 1 && grid[y][x + 1] != '#' {
+                neighbors += 1;
+            }
+            if y > 0 && grid[y - 1][x] != '#' {
+                neighbors += 1;
+            }
+            if y < grid.len() - 1 && grid[y + 1][x] != '#' {
+                neighbors += 1;
+            }
+            if neighbors > 2 {
+                if nodes_set.insert((x, y)) {
+                    nodes_vec.push((x, y));
+                    grid_to_node_ind.insert((x, y), nodes_vec.len() - 1);
+                }
+            }
+        }
+    }
+    nodes_vec.push((1, 0));
+    nodes_set.insert((1, 0));
+    let start_node_ind = nodes_vec.len() - 1;
+    grid_to_node_ind.insert((1, 0), start_node_ind);
+
+    let end_node_x = (0..grid[0].len()).find(|&x| grid[grid.len() - 1][x] == '.').unwrap();
+    nodes_vec.push((end_node_x, grid.len() - 1));
+    nodes_set.insert((end_node_x, grid.len() - 1));
+    grid_to_node_ind.insert((end_node_x, grid.len() - 1), nodes_vec.len() - 1);
+    let end_node_ind = nodes_vec.len() - 1;
+
+    // let's store graph as a hashmap of (node_ind) -> Vec<(node_ind, distance)>
+    let mut graph: HashMap<usize, Vec<(usize, usize)>> = HashMap::new();
+
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    // if we find a node, we need to add it to the graph
+    // and then continue the BFS
+    // queue will be (x, y, dist)
+    // let's just start bfs from every node
+    for prev_node_ind in 0..nodes_vec.len() {
+        let (x, y) = nodes_vec[prev_node_ind];
+        // find all adjacent nodes and break;
+        let mut queue: VecDeque<(usize, usize, usize)> = VecDeque::new();
+        queue.push_back((x, y, 0));
+
+        while let Some((x, y, dist)) = queue.pop_front() {
+            if nodes_set.contains(&(x, y)) {
+                // found a node, connect it to the previous node
+                let node_ind = grid_to_node_ind[&(x, y)];
+                if node_ind != prev_node_ind {
+                    graph.entry(prev_node_ind).or_insert_with(Vec::new).push((node_ind, dist));
+                    graph.entry(node_ind).or_insert_with(Vec::new).push((prev_node_ind, dist));
+                    continue;
+                }
+            }
+            if !visited.insert((x, y)) {
+                continue;
+            }
+            if x > 0 && grid[y][x - 1] != '#' {
+                queue.push_back((x - 1, y, dist + 1));
+            }
+            if x < grid[0].len() - 1 && grid[y][x + 1] != '#' {
+                queue.push_back((x + 1, y, dist + 1));
+            }
+            if y > 0 && grid[y - 1][x] != '#' {
+                queue.push_back((x, y - 1, dist + 1));
+            }
+            if y < grid.len() - 1 && grid[y + 1][x] != '#' {
+                queue.push_back((x, y + 1, dist + 1));
+            }
+        }
+    }
+
+    // now we have a graph, let's find the longest path
+    // we can brute force this
+
+    let mut max_path = 0;
+    // each path must have a visited set
+    // queue will be (node_ind, visited_set<(node_ind), path_dist>)
+    let mut queue: VecDeque<(usize, HashSet<usize>, usize)> = VecDeque::new();
+    queue.push_back((start_node_ind, HashSet::new(), 0));
+    while !queue.is_empty() {
+        let (node_ind, visited, path_dist) = queue.pop_front().unwrap();
+        //println!("node_ind: {}, path_dist: {}", node_ind, path_dist);
+        if node_ind == end_node_ind {
+            max_path = max(max_path, path_dist);
+            continue;
+        }
+        let mut visited = visited.clone();
+        if visited.insert(node_ind) {
+            for &(next_node_ind, dist) in &graph[&node_ind] {
+                queue.push_back((next_node_ind, visited.clone(), dist + path_dist));
+            }
+        }
+    }
+
+
+    println!("part2: {}", max_path/* - 1*/);
+}
